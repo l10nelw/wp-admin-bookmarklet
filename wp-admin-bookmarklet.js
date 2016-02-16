659d807e0e1cca6830d146939e29e247dac489b6
lionelster <lionelster@gmail.com>
2016-02-16 12:39:03 +0800

    accommodate localhost. better documentation

--- wp-admin-bookmarklet.js
+++ wp-admin-bookmarklet.js
@@ -1,1 +1,12 @@
-javascript:void(window.open('/wp-admin','_blank'));
+javascript:l=window.location;void(window.open(l.href.split('/').slice(0,3+(l.host=='localhost')).concat('wp-admin').join('/'),'_blank'));
+
+javascript:
+l = window.location;
+void(window.open(
+	l.href
+		.split('/')
+		.slice(0, 3 + (l.host == 'localhost')) // if localhost, slice 4 to include 1st level directory, else slice 3
+		.concat('wp-admin')
+		.join('/'),
+	'_blank')
+);
